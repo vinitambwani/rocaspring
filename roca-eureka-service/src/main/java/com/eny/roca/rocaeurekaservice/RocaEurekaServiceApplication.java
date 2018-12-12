f322fa2533f4a111385f485eae8198e144ae2adf
unknown <niravdhami@gmail.com>
2018-12-13 00:14:25 +0530

    #Addding Zuul Proxy#

--- roca-eureka-service/src/main/java/com/eny/roca/rocaeurekaservice/RocaEurekaServiceApplication.java
+++ roca-eureka-service/src/main/java/com/eny/roca/rocaeurekaservice/RocaEurekaServiceApplication.java
@@ -3,7 +3,9 @@ package com.eny.roca.rocaeurekaservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @EnableEurekaServer
 @SpringBootApplication
 public class RocaEurekaServiceApplication {
